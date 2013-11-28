19ca99c44f5cf1e64159e644e9bdedfd2063a8ed
Cameron <chuc@rpi.edu>
2013-11-27 19:28:58 -0500

    Spawns gray rectangle in 3d.

---
+++ code/include/runMode.h
@@ -0,0 +1,69 @@
+#include <stdio.h>
+//#include "include/domino.h"
+
+void domin(float x, float y)
+{
+    glPushMatrix();
+    glTranslatef(x,y,0);
+    glRotatef(90,1,0,0);
+    glScalef(.5,.5,1);
+    glutSolidCube(100);
+    glPopMatrix();
+}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    float pos[] = {200,200,0};
+    float dif[] = {.3,.3,.3,3};
+    float spe[] = {1,1,1,1};
+    float amb[] = {1,1,1,0};
+    glLightfv(GL_LIGHT0,GL_POSITION,pos);
+    glLightfv(GL_LIGHT0,GL_DIFFUSE,dif);
+    glLightfv(GL_LIGHT0,GL_AMBIENT,amb);
+    glLightfv(GL_LIGHT0,GL_SPECULAR,spe);
+
+    glTranslatef(50,50,0);
+    //glRotatef(30,1,0,0);
+    //glRotatef(-30,0,1,0);
+
+    gluPerspective(0, 1, 0, 10);
+    
+    // creating the domino
+    // domino x = new domino(0,0,0);
+    domin(50,50);
+    domin(5,5);
+    domin(0,0);
+    domin(-40,-40);
+    glFlush();
+}
+void reshape(int w,int h)
+{
+    glViewport(0,0,w,h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0,400,0,400,-400,400);
+    glMatrixMode(GL_MODELVIEW);
+    //gluLookAt(0,  0,  0,  0,  0,  0,  0,  0,  0);
+}
+
+void run_mode_display()
+{
+    //glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE | GLUT_DEPTH);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(400,400);
+    glutCreateWindow("runmode.h");
+    glClearColor(1,1,1,1);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_NORMALIZE);
+
+
+    glShadeModel(GL_SMOOTH);
+    glutReshapeFunc(reshape);
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+}
