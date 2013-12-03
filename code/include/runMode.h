c23c9290cd1ae20db9e095c61248a48e957ae59a
Wesley Miller <millew2@rpi.edu>
2013-12-03 17:39:59 -0500

    camera work

--- code/include/runMode.h
+++ code/include/runMode.h
@@ -2,6 +2,11 @@
 
 void make_floor()
 {   
+// Material property vectors.
+   float matSpec[] = { 0.0, 1.0, 1.0, 1.0 };
+   float matShine[] = { 50.0 };
+   float matAmbAndDif[] = {0.0, 0.1, 1.0, 1.0};
+
     float t = 1000*WIDTH;
     
   //glClear(GL_COLOR_BUFFER_BIT);
@@ -9,8 +14,8 @@ void make_floor()
   // Draw a blue grid 
   glColor3f(1, 0, 0);
   glBegin(GL_QUADS);
-    glVertex3f(-t, -t, 0); glVertex3f(t, -t, 0);
-    glVertex3f(t, t, 0); glVertex3f(-t, t, 0);
+    glVertex3f(0, 0, 0); glVertex3f(t, 0, 0);
+    glVertex3f(t, t, 0); glVertex3f(0, t, 0);
   /*
   for (GLfloat i = -t; i <= t; i += t/1000) {
     glVertex3f(i, t, 0); glVertex3f(i, -t, 0);
@@ -25,11 +30,16 @@ void make_floor()
 
 void draw_domino(domino d)
 {
+	// Material property vectors.
+   float matSpec[] = { 1.0, 1.0, 0.0, 1.0 };
+   float matShine[] = { 50.0 };
+   float matAmbAndDif[] = {1.0, 0.1, 0.0, 1.0};
+
   float W = WIDTH/2;
   float H = HEIGHT/2;
    
-    float x = d.getX()-W;
-    float y = d.getY()-H;
+    float x = d.getX();
+    float y = d.getY();
     float tilt = d.getTilt();
     float norm = d.getNorm()*180/3.14;
     //if (norm >= 100) 
@@ -83,8 +93,15 @@ void rdraw(void)
 
     //if(dominoes.size()>1)gluLookAt(0.0, 0.0, 50, dominoes[0].getX(), dominoes[0].getY(), 0.0, 0, 1, 0);
     // need 2 to give it perspective, or maybe just one done right
-    gluLookAt(10.0, 10.0, -14.0, 0.0, 0.0, 0.0, 0.0, 0, -1); 
-    gluLookAt(0.0, 0.0, 0.0, WIDTH/2, HEIGHT/2, 5.0, 0, 0, -1);
+    //gluLookAt(10.0, 10.0, -14.0, 0.0, 0.0, 0.0, 0.0, 0, -1); 
+    if(dominoes.size())
+    {
+    	gluLookAt(lead -> getX() - 25, lead -> getY() - 25, -25, lead -> getX(), lead -> getY(), 0, 0, 0, -1);
+	}
+	else
+	{
+		gluLookAt(-25, -25, -25, 0, 0, 0, 0, 0, -1);
+	}
     
     make_floor();
     
