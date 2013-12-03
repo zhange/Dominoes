42075bf93cd22e10749a8c399c8bc13cd7423b85
Cameron <chuc3@rpi.edu>
2013-12-03 14:59:42 -0500

    changed axis to z

--- code/include/runMode.h
+++ code/include/runMode.h
@@ -22,6 +22,8 @@ void draw_domino(domino d)
     //if (norm >= 100) 
     //norm = 90;
     
+    // normalize stuff //
+    /*
     if ( (norm<=1 && norm >=-1) || (norm-180<=1 && norm-180>=-1) ) norm = 90;
     else{
     if (norm <= 0) norm+=180;
@@ -29,6 +31,7 @@ void draw_domino(domino d)
     if (norm >90) norm -=90;
     if (norm <=0) norm+=90;
     }
+    */
     cout << (int)norm << "  ";
     glPushMatrix();
     // rotate then translate...
@@ -39,14 +42,14 @@ void draw_domino(domino d)
     
     // put two rotations in here, one for tilt one for forward
     
-    glRotatef(norm, 0 , 1 , 0 );
-    glRotatef(tilt, 1,  0 , 0 );
+    glRotatef(norm, 0 , 0 , 1 );
+    glRotatef(tilt, 0,  1 , 0 );
     //glScalef(1.5, 2.5, .5); //glScalef(.025,.025,.025);
-    glScalef(10,25,3);
+    glScalef(5,15,25);
     //glScalef(.5,.5,.5);
     //glColor3f(1,0,0);
-    //glutSolidCube(1);
-    glutWireCube(1);
+    //glutSolidCube(.75);
+    glutWireCube(.75);
     glPopMatrix();
 }
 //Drawing funciton
@@ -65,9 +68,9 @@ void rdraw(void)
     //if(dominos.size()>1)gluLookAt(0.0, 0.0, 50, dominos[0].getX(), dominos[0].getY(), 0.0, 0, 1, 0);
     glTranslatef( 0, 0, -300 );
     //if(dominos.size()>1)gluLookAt(0.0, 0.0, 50, dominos[0].getX(), dominos[0].getY(), 0.0, 0, 1, 0);
-    gluLookAt(10.0, 10.0, 14.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); 
+    gluLookAt(10.0, 10.0, 14.0, 0.0, 0.0, 0.0, 0.0, 0, -1); 
     
-    gluLookAt(0.0, 0.0, 0.0, WIDTH/2, HEIGHT/2, 5.0, 0, 1, 0);
+    gluLookAt(0.0, 0.0, 0.0, WIDTH/2, HEIGHT/2, 5.0, 0, 0, -1);
   for (int i = 0; i < dominos.size(); i++)
   {
     draw_domino(dominos[i]);
