5b03bab855b4a3434ebdcedeecbc490c5a2c9a96
Cameron <chuc3@rpi.edu>
2013-12-03 14:38:24 -0500

    fixed normals

--- code/include/runMode.h
+++ code/include/runMode.h
@@ -19,14 +19,28 @@ void draw_domino(domino d)
     float y = d.getY()-H;
     float tilt = d.getTilt();
     float norm = d.getNorm()*180/3.14;
+    //if (norm >= 100) 
+    //norm = 90;
     
+    if ( (norm<=1 && norm >=-1) || (norm-180<=1 && norm-180>=-1) ) norm = 90;
+    else{
+    if (norm <= 0) norm+=180;
+    else norm-=90;
+    if (norm >90) norm -=90;
+    if (norm <=0) norm+=90;
+    }
+    cout << (int)norm << "  ";
     glPushMatrix();
+    // rotate then translate...
+    
+
+    //glRotatef(45,forward,rotation,0); // do rotation before translation
+    glTranslatef(x,y,0);    
     
     // put two rotations in here, one for tilt one for forward
-    glRotatef(tilt, 1 , 0 , 0 );
+    
     glRotatef(norm, 0 , 1 , 0 );
-    //glRotatef(45,forward,rotation,0); // do rotation before translation
-    glTranslatef(x,y,0);
+    glRotatef(tilt, 1,  0 , 0 );
     //glScalef(1.5, 2.5, .5); //glScalef(.025,.025,.025);
     glScalef(10,25,3);
     //glScalef(.5,.5,.5);
@@ -52,10 +66,12 @@ void rdraw(void)
     glTranslatef( 0, 0, -300 );
     //if(dominos.size()>1)gluLookAt(0.0, 0.0, 50, dominos[0].getX(), dominos[0].getY(), 0.0, 0, 1, 0);
     gluLookAt(10.0, 10.0, 14.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); 
+    
+    gluLookAt(0.0, 0.0, 0.0, WIDTH/2, HEIGHT/2, 5.0, 0, 1, 0);
   for (int i = 0; i < dominos.size(); i++)
   {
     draw_domino(dominos[i]);
-    cout << dominos[i].getX() << " " << dominos[i].getY() << " normal: " << dominos[i].getNorm()*180/3.14 << endl;
+    cout << dominos[i].getX() << " " << dominos[i].getY() << " normal: " << endl;//dominos[i].getNorm()*180/3.14 << endl;
   }
   //if(dominos.size()>1)gluLookAt(dominos[0].getX(), dominos[0].getY(), 250, dominos[0].getX(), dominos[0].getY(), 0.0, 0, 1, 0);
   // look at first domino ?
--- code/main.cpp
+++ code/main.cpp
@@ -326,3 +326,5 @@ void mouseControl(int button, int state, int x, int y)
 }
 
 
+
+
