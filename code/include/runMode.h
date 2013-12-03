e840e2db8fda443b56b52ea806d5d7f90a69fae1
Cameron <chuc3@rpi.edu>
2013-12-02 22:52:07 -0500

    vertices replacing cube

--- code/include/runMode.h
+++ code/include/runMode.h
@@ -1,11 +1,14 @@
 
 void init()
 {
+/*
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
     glEnable(GL_COLOR_MATERIAL);
+*/
 }
+
 
 void draw_domino(domino d)
 {
@@ -26,7 +29,49 @@ void draw_domino(domino d)
     glTranslatef(x,y,0);
     glScalef(1.5, 2.5, .5); //glScalef(.025,.025,.025);
     glColor3f(1,0,0);
-    glutSolidCube(5);
+            
+            glBegin(GL_QUADS);
+            glNormal3d(  0,      0,   1 );
+            glVertex3f(  0.5, -0.5, 0.5 );
+            glVertex3f(  0.5,  0.5, 0.5 );
+            glVertex3f( -0.5,  0.5, 0.5 );
+            glVertex3f( -0.5, -0.5, 0.5 );
+            glEnd();
+             
+            // Purple side - RIGHT
+            glBegin(GL_QUADS);
+            glVertex3f( 0.5, -0.5, -0.5 );
+            glVertex3f( 0.5,  0.5, -0.5 );
+            glVertex3f( 0.5,  0.5,  0.5 );
+            glVertex3f( 0.5, -0.5,  0.5 );
+            glEnd();
+             
+            // Green side - LEFT
+            glBegin(GL_QUADS);
+            glVertex3f( -0.5, -0.5,  0.5 );
+            glVertex3f( -0.5,  0.5,  0.5 );
+            glVertex3f( -0.5,  0.5, -0.5 );
+            glVertex3f( -0.5, -0.5, -0.5 );
+            glEnd();
+             
+            // Blue side - TOP
+            glBegin(GL_QUADS);
+            glVertex3f(  0.5,  0.5,  0.5 );
+            glVertex3f(  0.5,  0.5, -0.5 );
+            glVertex3f( -0.5,  0.5, -0.5 );
+            glVertex3f( -0.5,  0.5,  0.5 );
+            glEnd();
+             
+            // Red side - BOTTOM
+            glBegin(GL_QUADS);
+            glVertex3f(  0.5, -0.5, -0.5 );
+            glVertex3f(  0.5, -0.5,  0.5 );
+            glVertex3f( -0.5, -0.5,  0.5 );
+            glVertex3f( -0.5, -0.5, -0.5 );
+            glEnd();
+             
+            glFlush();
+            glutSwapBuffers();
     glPopMatrix();
 }
 //Drawing funciton
@@ -43,14 +88,14 @@ void rdraw(void)
     // move back a bit
     
     //if(dominos.size()>1)gluLookAt(0.0, 0.0, 50, dominos[0].getX(), dominos[0].getY(), 0.0, 0, 1, 0);
-    glTranslatef( 0, 0, -100 );
+    glTranslatef( 0, 0, -250 );
     //if(dominos.size()>1)gluLookAt(0.0, 0.0, 50, dominos[0].getX(), dominos[0].getY(), 0.0, 0, 1, 0);
   for (int i = 0; i < dominos.size(); i++)
   {
     draw_domino(dominos[i]);
     cout << dominos[i].getX() << " " << dominos[i].getY() << " normal: " << dominos[i].getNorm() << endl;
   }
-  if(dominos.size()>1)gluLookAt(0.0, 0.0, 50, dominos[0].getX(), dominos[0].getY(), 0.0, 0, 1, 0);
+  //if(dominos.size()>1)gluLookAt(dominos[0].getX(), dominos[0].getY(), 250, dominos[0].getX(), dominos[0].getY(), 0.0, 0, 1, 0);
   // look at first domino ?
   //gluLookAt(0.0, 0.0, 5.0, x.getX(), x.getY(), 0.0, 0.0, 0.0, 1.0);
   
